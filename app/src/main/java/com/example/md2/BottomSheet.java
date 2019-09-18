e8d81d42bed5386332c0f38405c73aaf3502469e
shadowoffox <denisochnev@mail.ru>
2019-09-19 01:22:45 +0300

    first

---
+++ app/src/main/java/com/example/md2/BottomSheet.java
@@ -0,0 +1,14 @@
+package com.example.md2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomSheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet);
+    }
+}
